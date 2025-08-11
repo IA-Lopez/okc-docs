716450ca7e39e0d1ba6badabc6ee4189421a5bcb
mozilla-b2g <83683667+IA-Lopez@users.noreply.github.com>
2025-08-10 18:14:50 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,5 +6,5 @@ release = '1.0'
 extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-html_theme = 'readthedocs'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
